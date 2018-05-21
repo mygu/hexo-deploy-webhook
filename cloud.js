bfdf6f7a639cdb705876415834d43e2c8437027b
Mingyu Gu <mingyugu0410@gmail.com>
2018-05-21 12:39:18 +0800

    fixed cloud function can not work issue

--- cloud.js
+++ cloud.js
@@ -4,6 +4,9 @@ var handler = require('./public/js/handler');
 /**
  * Hexo博客发布后触发自动编译部署的钩子程序
  */
-AV.Cloud.define('hook', function (request) {
-    return handler();
+AV.Cloud.define('hook', function (req, res) {
+    handler();
+    res.status(200).json({
+        message: 'success'
+    });
 });
